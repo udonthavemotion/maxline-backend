d75f131ebe986415eb34398feeaa05f0c4f0c759
udonthavemotion <zeromotionprofits@outlook.com>
2025-07-21 15:03:54 -0500

    Fix Supabase SSL configuration for deployment

--- config/database.ts
+++ config/database.ts
@@ -31,12 +31,7 @@ export default ({ env }) => {
         user: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'postgres'),
         ssl: env.bool('DATABASE_SSL', false) ? {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+          rejectUnauthorized: false,
         } : false,
         schema: env('DATABASE_SCHEMA', 'public'),
       },
