75180c34f1802d7122448bec8a3c94862d95ca89
udonthavemotion <zeromotionprofits@outlook.com>
2025-07-22 19:22:29 -0500

    Backend ready for cloud deployment

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://majestic-love-3852ac722d.media.strapiapp.com'),
+  url: env('PUBLIC_URL', 'https://fantastic-thrill-37a105061e.strapiapp.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
